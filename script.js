aa9ae86804cbf954b51b4f62278b53bd6ff5abe2
Putra1906 <101515568+Putra1906@users.noreply.github.com>
2024-07-04 08:30:09 +0700

    deploy

---
+++ script.js
@@ -0,0 +1,40 @@
+var typed = new Typed(".text-mbclab", {
+  strings :["MBC LAB ?"],
+  typeSpeed: 130,
+  backSpeed: 100,
+  backDelay: 900,
+  loop:true
+})
+
+var typed = new Typed(".div-cs", {
+  strings :["CYBER SECURITY"],
+  typeSpeed: 130,
+  backSpeed: 100,
+  backDelay: 900,
+  loop:true
+})
+
+var typed = new Typed(".div-bd", {
+  strings :["BIG DATA"],
+  typeSpeed: 130,
+  backSpeed: 100,
+  backDelay: 900,
+  loop:true
+})
+
+var typed = new Typed(".div-gt", {
+  strings :["GAME TECH"],
+  typeSpeed: 130,
+  backSpeed: 100,
+  backDelay: 900,
+  loop:true
+})
+
+var typed = new Typed(".div-gis", {
+  strings :["GEOGRAPHIC INFORMATION SYSTEM"],
+  typeSpeed: 130,
+  backSpeed: 100,
+  backDelay: 900,
+  loop:true
+})
+
